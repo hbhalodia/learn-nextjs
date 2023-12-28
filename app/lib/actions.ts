04fcb94128c7c901792452355e40d2e10a405a80
hbhalodia <hit.bhalodia@rtcamp.com>
2023-12-28 11:30:20 +0530

    Add authentication and metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 export type State = {
 	errors?: {
 		customerId?: string[];
@@ -62,12 +65,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
 	redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
+
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Update Invoice.',
+		};
+	}
+
+	const { customerId, amount, status } = validatedFields.data;
 
 	const amountInCents = amount * 100;
 
@@ -92,5 +105,24 @@ export async function deleteInvoice(id: string) {
 		return { message: 'Deleted Invoice.' };
 	} catch (error) {
 		return { message: 'Database Error: Failed to Delete Invoice.' };
+	}
+}
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData,
+) {
+	try {
+		await signIn('credentials', formData);
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'Invalid credentials.';
+				default:
+					return 'Something went wrong.';
+			}
+		}
+		throw error;
 	}
 }
